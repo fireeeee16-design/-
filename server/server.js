f8610949998d14303743437b641c749453b4ad31
Abdul <fireeeee16@gmail.com>
2025-12-19 15:09:15 +0300

    Исправлен package.json с нужными зависимостями

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const emailTemplates = require('./emailTemplates');
 const { createTransporter, sendEmail } = require('./emailConfig');
 const express = require('express');
